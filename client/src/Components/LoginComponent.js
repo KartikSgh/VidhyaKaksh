e145f8c8fd975971300983c532fa534f9ae49d26
KartikSgh <2019ucp1394@mnit.ac.in>
2021-12-18 23:30:12 +0530

    added bootstrap css and react-bootstrap

--- client/src/Components/LoginComponent.js
+++ client/src/Components/LoginComponent.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
 import "../CSS/login.css";
 const LoginComponent = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <Link to="/home" className="btn btn-primary">
+        Login
+      </Link>
+    </div>
+  );
 };
 export default LoginComponent;
