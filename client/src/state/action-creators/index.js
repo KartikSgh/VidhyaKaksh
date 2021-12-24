3a9628dfc189830a0a88a70aec35b87b99947bd3
KartikSgh <2019ucp1394@mnit.ac.in>
2021-12-24 22:11:19 +0530

    completed home component

--- client/src/state/action-creators/index.js
+++ client/src/state/action-creators/index.js
@@ -15,3 +15,48 @@ export const testSubtract = (value) => {
     });
   };
 };
+
+export const changeEmail = (email) => {
+  return (dispatch) => {
+    dispatch({
+      type: "changeEmail",
+      payload: email,
+    });
+  };
+};
+
+export const changeClassCode = (code) => {
+  return (dispatch) => {
+    dispatch({
+      type: "changeClassCode",
+      payload: code,
+    });
+  };
+};
+
+export const addClassCard = (card) => {
+  return (dispatch) => {
+    dispatch({
+      type: "addClassCard",
+      payload: card,
+    });
+  };
+};
+
+export const updateClassCardList = (cards) => {
+  return (dispatch) => {
+    dispatch({
+      type: "updateClassCardList",
+      payload: cards,
+    });
+  };
+};
+
+export const deleteClassCard = (cardId) => {
+  return (dispatch) => {
+    dispatch({
+      type: "deleteClassCard",
+      payload: cardId,
+    });
+  };
+};
